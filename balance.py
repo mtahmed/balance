e19b96863d30c0bfa32c36fccc5efa3d64003f2b
Muhammad Tauqir <mtahmed@uwaterloo.ca>
2013-01-24 22:15:57 -0500

    Add the code already in use.
    TODO: Clean code and add comments.

---
+++ balance.py
@@ -0,0 +1,349 @@
+#!/usr/bin/env python
+
+# Standard imports
+import sys
+import os
+import cgi
+import cgitb
+import re
+import MySQLdb
+
+# Custom imports
+import settings
+
+cgitb.enable()
+
+print "Content-type: text/html"
+print
+username = os.environ['REMOTE_USER']
+
+conn = MySQLdb.connect(host = settings.database_hostname,
+                       user = settings.database_user,
+                       passwd = settings.database_password,
+                       db = settings.database_name)
+cursor = conn.cursor()
+
+
+def log(entry):
+    '''
+    Add the user with name NAME to the database.
+    '''
+    cursor.execute('''INSERT INTO balance_logs VALUES (NOW(), %s, %s)''', (username, entry))
+
+
+def add_user(name):
+    '''
+    Add the user with name NAME to the database.
+    '''
+    users = get_all_users()
+    if name in users:
+        raise Exception("The user %s is already in the database." % name)
+    cursor.execute('''INSERT INTO users VALUES (%s)''', (name,))
+    for user in users:
+        create_new_balance(name, user)
+    return
+
+
+def get_all_users():
+    '''
+    Get a list of all the users we have in the databse.
+    '''
+    cursor.execute('''SELECT name FROM users''')
+    rows = cursor.fetchall()
+    return [row[0] for row in rows]
+
+
+def create_new_balance(person1, person2):
+    '''
+    Create an empty record for PERSON1 to PERSON2 and the other way.
+    '''
+    cursor.execute('''INSERT INTO balance VALUES (%s, %s, %s)''', (person1, person2, 0))
+    cursor.execute('''INSERT INTO balance VALUES (%s, %s, %s)''', (person2, person1, 0))
+    return
+
+
+def get_balance(person1, person2):
+    '''
+    Get the amount PERSON1 owes PERSON2.
+    '''
+    cursor.execute('''SELECT amount FROM balance WHERE person1=%s AND person2=%s''', (person1, person2))
+    return cursor.fetchone()[0]
+
+
+def update_balance(person1, person2, amount_str):
+    '''
+    Update the AMOUNT that PERSON1 owes PERSON2.
+    '''
+    if amount_str == 'none':
+        amount = 0
+    else:
+        amount = float(amount_str)
+    if amount_str[0] in ('+', '-'):
+        person1_to_person2 = get_balance(person1, person2)
+        if person1_to_person2 + amount > 0.0:
+            cursor.execute('''UPDATE balance SET amount=%s WHERE person1=%s AND person2=%s''',
+                        (person1_to_person2 + amount, person1, person2))
+        else:
+            cursor.execute('''UPDATE balance SET amount=%s WHERE person1=%s AND person2=%s''',
+                        (0.0, person1, person2))
+            cursor.execute('''UPDATE balance SET amount=%s WHERE person1=%s AND person2=%s''',
+                        (-1 * (person1_to_person2 + amount), person2, person1))
+    else:
+        cursor.execute('''UPDATE balance SET amount=%s WHERE person1=%s AND person2=%s''',
+                    (amount, person1, person2))
+    
+
+def print_table():
+    users = get_all_users()
+
+    print """<h1>Balance</h1>"""
+    print """<div id='balance-table' style='margin: 0px; padding: 0px;'>"""
+    print """<table>"""
+    print """<tr>"""
+    print """<td></td>"""
+    for user in users:
+        print """<td>%s</td>""" % user
+    print """</tr>"""
+
+    for person1 in users:
+        print """<tr>"""
+        print """<td>%s</td>""" % person1
+        for person2 in users:
+            if person1 == person2:
+                print """<td></td>"""
+                continue
+            person1_to_person2 = get_balance(person1, person2)
+            if person1_to_person2 == 0.0 or person1_to_person2 is None:
+                print """<td></td>"""
+            else:
+                print """<td style='text-align: right;'>%.2f</td>""" % person1_to_person2
+        print """</tr>"""
+    
+    print """</table>"""
+    print """</div>"""
+
+
+def print_logs():
+    cursor.execute('''SELECT * FROM balance_logs ORDER BY ts DESC LIMIT 50''')
+    logs = cursor.fetchall()
+    print """<h1>Logs</h1>"""
+    print """<table>"""
+    print """<tr><td>date</td><td>user</td><td>entry</td></tr>"""
+    for log in logs:
+        print """<tr>"""
+        print """<td>%s</td>""" % log[0]
+        print """<td>%s</td>""" % log[1]
+        print """<td>%s</td>""" % log[2]
+        print """</tr>"""
+    print """</table>"""
+    print """<br />"""
+    print """<br />"""
+    
+
+
+def print_form():
+    users = get_all_users()
+
+    print """
+    <div style='margin-top: 15px;'>
+        <input type='text' id='command-box' style='width: 296px; padding: 0px;' /> [Enter to submit]
+        <br />
+        <div id='status' style='height: 15px;  width: 300px; background-color: #ddd;'></div>
+        <br />
+        <br />
+    """
+
+
+def print_examples():
+    print """
+        <h1>Examples</h1>
+        szbokhar owes mtahmed $10:
+        <br />
+        <code>szbokhar owes mtahmed 10</code>
+        <br />
+        <br />
+        szbokhar owes mtahmed $10 more:
+        <br />
+        <code>szbokhar owes mtahmed +10</code>
+        <br />
+        <br />
+        szbokhar pays back mtahmed $10:
+        <br />
+        <code>szbokhar owes mtahmed -10</code>
+        <br />
+        <br />
+        szbokhar pays back mtahmed all that he owes:
+        <br />
+        <code>szbokhar owes mtahmed 0</code>
+        <br />
+        or
+        <br />
+        <code>szbokhar owes mtahmed none</code>
+        <br />
+        <br />
+        <!--
+        The command line also accepts unambigious prefixes of usernames.
+        <br />
+        So all these are equivalent (for users: [mtahmed, szbokhar, my4li])
+        <br />
+        <code>szbokhar owes mtahmed 0</code>
+        <br />
+        <code>szbokhar owes mt 0</code>
+        <br />
+        <code>s owes mt 0</code>
+        <br />
+        <code>sz owes mt 0</code>
+        <br />
+        <br />
+        -->
+        The command line also accepts lists.
+        <br />
+        Everyone owes mtahmed +$2
+        <br />
+        <code>szbokhar my4li blung owes mtahmed +2</code>
+        <br />
+        <br />
+        The above can also be written using the 'all' keyword.
+        <br />
+        <code>all owes mtahmed +2</code>
+        <br />
+        <br />
+        The 'for' keyword can be used to log reasons for updates.
+        <br />
+        <code>all owes mtahmed +10 for pizza</code>
+        <br />
+        <code>szbokhar owes mtahmed -10 for paid back</code>
+    </div>
+    """
+
+
+def print_body_head():
+    print """
+    <!DOCTYPE HTML>
+
+    <html>
+
+    <head>
+    <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js'></script>
+    <script src='https://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js'></script>
+    <script src='https://csclub.uwaterloo.ca/~mtahmed/balance/commands.js'></script>
+    <link rel='stylesheet' type='text/css' href='https://www.csclub.uwaterloo.ca/~mtahmed/css/style-main.css' />
+
+    <style type='text/css'>
+        body {
+            font-size: 12px;
+        }
+        table {
+            border-collapse: collapse;
+            padding: 0px;
+            margin: 0px;
+        }
+        tr {
+            padding: 0px;
+            margin: 0px;
+        }
+        td {
+            padding: 5px;
+        }
+        table, th, td {
+            border: 1px solid #999;
+        }
+        code {
+            padding: 2px;
+            font-family: monospace;
+            background-color: #CCC;
+        }
+    </style>
+    </head>
+
+    <body>
+    """
+    print "User: %s" % username
+    print "<br />"
+    print "<br />"
+
+
+def print_body_foot():    
+    print """
+    </body>
+
+    </html>
+    """
+
+
+if __name__ == '__main__':
+    if username not in get_all_users() and username != 'mtahmed':
+        print "Permission denied for user: %s" % username
+        sys.exit(0)
+    params = cgi.FieldStorage()
+    command_str = ''
+    if 'command' in params:
+        command_str = params['command'].value
+
+    users = get_all_users()
+    
+    for_index = command_str.find('for')
+    if for_index == -1:
+        command_split = command_str.split(' ')
+    else:
+        command_split = command_str[:for_index - 1].split(' ')
+    if command_split[0] == 'add':
+        add_list = command_split[1:]
+        for user in add_list:
+            add_user(user)
+        log(command_str)
+        print_table()
+    elif command_split[0] == '':
+        print_body_head()
+        print_table()
+        print_form()
+        print_logs()
+        print_examples()
+        print_body_foot()
+    else:
+        owes_index = command_split.index('owes')
+        if command_split[0] == 'all':
+            from_list = users
+        else:
+            from_list = command_split[:owes_index]
+        to_list = command_split[owes_index + 1:len(command_split) - 1]
+        # The user could be using prefixes of names; in that case, determine the actual name.
+        for index, from_user in enumerate(from_list):
+            if from_user in users:
+                continue
+            matches = []
+            pattern = re.compile(from_user)
+            for user in users:
+                if pattern.match(user):
+                    matches.append(user)
+            if len(matches) > 1:
+                print "error: mbigious name: %s" % from_user
+                exit(0)
+            elif len(matches) == 0:
+                print "error: no matches for name: %s" % from_user
+                exit(0)
+            else:
+                from_list[index] = matches[0]
+        for index, to_user in enumerate(to_list):
+            if to_user in users:
+                continue
+            matches = []
+            pattern = re.compile(to_user)
+            for user in users:
+                if pattern.match(user):
+                    matches.append(user)
+            if len(matches) > 1:
+                print "error: mbigious name: %s" % to_user
+                exit(0)
+            elif len(matches) == 0:
+                print "error: no matches for name: %s" % to_user
+                exit(0)
+            else:
+                to_list[index] = matches[0]
+
+        amount_str = command_split[-1]
+        for person1 in from_list:
+            for person2 in to_list:
+                update_balance(person1, person2, amount_str)
+        log(command_str)
+        print_table()
---
+++ commands.js
@@ -0,0 +1,23 @@
+$(document).ready(
+  function() {
+    $('#command-box').focus();
+    $('#command-box').keyup(
+      function(e) {
+        if (e.keyCode == 13) {
+          $.get('balance.py?command=' + encodeURIComponent($(this).val()),
+            function(data) {
+              if (data.substring(0, 5) != 'error') {
+                $('#status').html('done').delay(5000).queue(function() { $(this).html('') });
+                $('#balance-table').html(data);
+              } else {
+                alert(data);
+              }
+              $('#command-box').val('');
+            }
+          );
+          return false;
+        }
+      }
+    );
+  }
+);
