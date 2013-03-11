590ae592ab06c772aed3761e5e709bc9dc35819b
Muhammad Tauqir <mtahmed@uwaterloo.ca>
2013-03-11 01:37:19 -0400

    Add a database_schema.sql file to deploy empty tables. Add .hgignore to ignore .pyc files. Update README.

---
+++ database_schema.sql
@@ -0,0 +1,35 @@
+--
+-- Table structure for table `balance`
+--
+
+DROP TABLE IF EXISTS `balance`;
+CREATE TABLE `balance` (
+  `from_user` text,
+  `to_user` text,
+  `amount` double DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `balance_logs`
+--
+
+DROP TABLE IF EXISTS `balance_logs`;
+CREATE TABLE `balance_logs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user` text,
+  `from_user` text,
+  `to_user` text,
+  `for_message` text,
+  `amount` double DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `name` text
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
